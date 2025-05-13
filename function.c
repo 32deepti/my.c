dcbb9dd39a25d27352f1ff43ef71397ca24f24cd
32deepti <deeptidodamani1@gmail.com>
2025-05-13 15:46:09 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int shridevi()
+ {
+     printf("name=deepti\n");
+     printf("DOB\n");
+     printf("age\n");
+     printf("mobile no\n");
+     
+ }
+int main()
+{
+    shridevi();
+    return 0;
+}
+
+   
